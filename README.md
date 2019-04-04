a0359e98ce749769e2e43256adf6bdb71328f244
Elkeborr <elkeborreij@student.thomasmore.be>
2019-04-04 16:30:29 +0200

    Readme_aangepast

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Php
-Php eindwerk.
-## Stap 1
-Pull de map binnen  🥑
-## Stap 2
-Codeer 💻
-## Stap 3
-Commit met een duidelijke naam 🤓
-## Stap 4
-Push 🥊
+Php eindwerk. Het readme bestand is een bestand om zaken duidelijk in te maken.
+Bijvoorbeeld de manier van klasses benoemen. Zet hier dus alle code gerelateerde specificaties.
+## Stappen
+Stap 1: fetch origin 🧶
+Stap 2: Pull de map binnen  🥑
+Stap 3: Codeer 💻
+Stap 4: Commit met een duidelijke naam 🤓
+Stap 5: Push 🥊
+
+
---
+++ register.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Registreer</title>
+</head>
+<body>
+<div class="form form--login">
+			<form action="" method="post">
+				<h2 form__title>Sign up for an account</h2>
+				<div class="form__field">
+					<label for="email">Email</label>
+					<input type="text" id="email" name="email">
+                </div>
+                <div class="form__field">
+					<label for="firstname">Firstname</label>
+					<input type="text" id="email" name="email">
+                </div>
+                <div class="form__field">
+					<label for="lastname">Lastname</label>
+					<input type="text" id="email" name="email">
+				</div>
+				<div class="form__field">
+					<label for="password">Password</label>
+					<input type="password" id="password" name="password">
+				</div>
+
+                <div class="form__field">
+					<label for="password_confirmation">Confirm your password</label>
+					<input type="password" id="password_confirmation" name="password_confirmation">
+				</div>
+
+				<div class="form__field">
+					<input type="submit" value="Sign me up!" class="btn btn--primary">	
+				</div>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
