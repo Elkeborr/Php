739d2656bda0a3897a8d4af7b03bc894e3f2f137
Elkeborr <elkeborreij@student.thomasmore.be>
2019-04-04 16:55:31 +0200

    update_readme

--- README.md
+++ README.md
@@ -8,4 +8,32 @@ Stap 3: Codeer 💻
 Stap 4: Commit met een duidelijke naam 🤓
 Stap 5: Push 🥊
 
+##Hacken
+
+Als Joris in ons systeem kan hacken verliezen we -5.
+
+### SQL-injectie
+
+SQl typen in een bepaald veld.
+Tegen gaan: real_escape_string()
+BV; $email = $conn -> real_escape_string($_POST['Email']);
+
+### XXS
+
+Aanval op javascript
+Tegen gaan: htmlspecialchars();
+BV; echo htmlspecialchars($user[firstName]);
+
+### Sniffing
+
+Tegen gaan : https
+
+### Bruth force
+
+Lijst van wachtwoorden en usernames laten loopen 
+Tegen gaan: 3 wachtwoord mogelijkheden 
+
+### databank beveiligen
+(Dit doen we wel pas op het einde)
+een @ symbool => @new mysqli("localhost","root", "root", "project");
 
---
+++ classes/User.php
@@ -0,0 +1,132 @@
+<?php
+
+class User {
+
+private $email;
+private $firstName;
+private $lastName;
+private $userName;
+private $password;
+private $passwordConformation;
+
+
+/**
+ * Get the value of email
+ */ 
+public function getEmail()
+{
+return $this->email;
+}
+
+/**
+ * Set the value of email
+ *
+ * @return  self
+ */ 
+public function setEmail($email)
+{
+$this->email = $email;
+
+return $this;
+}
+
+/**
+ * Get the value of firstName
+ */ 
+public function getFirstName()
+{
+return $this->firstName;
+}
+
+/**
+ * Set the value of firstName
+ *
+ * @return  self
+ */ 
+public function setFirstName($firstName)
+{
+$this->firstName = $firstName;
+
+return $this;
+}
+
+/**
+ * Get the value of lastName
+ */ 
+public function getLastName()
+{
+return $this->lastName;
+}
+
+/**
+ * Set the value of lastName
+ *
+ * @return  self
+ */ 
+public function setLastName($lastName)
+{
+$this->lastName = $lastName;
+
+return $this;
+}
+
+/**
+ * Get the value of userName
+ */ 
+public function getUserName()
+{
+return $this->userName;
+}
+
+/**
+ * Set the value of userName
+ *
+ * @return  self
+ */ 
+public function setUserName($userName)
+{
+$this->userName = $userName;
+
+return $this;
+}
+
+/**
+ * Get the value of password
+ */ 
+public function getPassword()
+{
+return $this->password;
+}
+
+/**
+ * Set the value of password
+ *
+ * @return  self
+ */ 
+public function setPassword($password)
+{
+$this->password = $password;
+
+return $this;
+}
+
+/**
+ * Get the value of passwordConformation
+ */ 
+public function getPasswordConformation()
+{
+return $this->passwordConformation;
+}
+
+/**
+ * Set the value of passwordConformation
+ *
+ * @return  self
+ */ 
+public function setPasswordConformation($passwordConformation)
+{
+$this->passwordConformation = $passwordConformation;
+
+return $this;
+}
+}
