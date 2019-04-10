c84f928f1347334df359e1a612f10838903041d9
serafimay <49272435+serafimay@users.noreply.github.com>
2019-04-10 17:19:09 +0200

    update profiel

--- profiel.php
+++ profiel.php
@@ -42,80 +42,69 @@ function test_input($data) {
 
 <h3>Profielfoto</h3>
       <div class="row">
-	   <?php
-	        $output_dir = "img/";
-		$allowedExts = array("jpg", "jpeg", "gif", "png","JPG");
-		$extension = @end(explode(".", $_FILES["myfile"]["name"]));
-		    if(isset($_POST['upload']))
-		    {
+<?php
 
-                if ((($_FILES["myfile"]["type"] == "image/gif")
-				    || ($_FILES["myfile"]["type"] == "image/jpeg")
-				    || ($_FILES["myfile"]["type"] == "image/JPG")
-				    || ($_FILES["myfile"]["type"] == "image/png")
-				    || ($_FILES["myfile"]["type"] == "image/pjpeg"))
-				    && ($_FILES["myfile"]["size"] < 504800)
-				    && in_array($extension, $allowedExts)) 
-			    {
-				      if ($_FILES["myfile"]["error"] > 0)
-					    {
-					    echo "Return Code: " . $_FILES["myfile"]["error"] . "<br>";
-					    }
-				    if (file_exists($output_dir. $_FILES["myfile"]["name"]))
-				      {
-				      unlink($output_dir. $_FILES["myfile"]["name"]);
-				      }	
-					    else
-					    {
-					    $pic=$_FILES["myfile"]["name"];
-					    $conv=explode(".",$pic);
-					    $ext=$conv['1'];	
-						    
-					    //move the uploaded file to uploads folder;
-				          move_uploaded_file($_FILES["myfile"]["tmp_name"],$output_dir.$user.".".$ext);
-					    
-					    $pics=$output_dir.$user.".".$ext;
-					  
-					      
-					    $url=$user.".".$ext;
-					    
-					    
-					    $update="update database set u_imgurl='$url' where u_user='$user'";
-					    
-					    if($sp->query($update)){
-						   echo '<div data-alert class="alert-box success radius">';
-						      echo  '<b>Success !</b>  Image Updated' ;
-						      echo  '<a href="#" class="close">&times;</a>';
-						    echo '</div>';
-						   header('refresh:3;url=dashboard.php'); 
-					    }
-					    else{
-						    echo '<div data-alert class="alert-box alert radius">';
-						      echo  '<b>Error !</b> ' .$sp->error ;
-						      echo  '<a href="#" class="close">&times;</a>';
-						    echo '</div>';
-					    }   
-					    }
-			    }	
-			    else{
-			    
-			       echo '<div data-alert class="alert-box warning radius">';
-			        echo  '<b>Warning !</b>  File not Uploaded, Check image' ;
-			        echo  '<a href="#" class="close">&times;</a>';
-				echo '</div>';
-		 
-			    }
-		    }	    
-	         ?>
-
-		<img src="img/<?php echo $info->u_imgurl; ?>" width="64" height="64" alt="User Image"/>
-		
-		    <form action="" method="post" enctype="multipart/form-data">
-		      <div class="large-12 columns">
-			<span style="color:red;">Maximun Image Size 100KB</span><br/><br/>
-			<input type="file" name="myfile"  required/>
-			<button type="submit" name="upload" class="tiny button radius success">Upload</button>
-		    </form>
+$user="database"; //you can fetch username here
+$db = new PDO("mysql:host=localhost, dbname=database", 'user', 'password', null);
+if($db->connect_errno){
+echo $db->connect_error;
+}
+$pull="select * from userimage where user='$user'";
+$allowedExts = array("jpg", "jpeg", "gif", "png","JPG");
+$extension = @end(explode(".", $_FILES["file"]["name"]));
+if(isset($_POST['pupload'])){
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/JPG")
+|| ($_FILES["file"]["type"] == "image/png")
+|| ($_FILES["file"]["type"] == "image/pjpeg"))
+&& ($_FILES["file"]["size"] < 200000)
+&& in_array($extension, $allowedExts))
+{
+ if ($_FILES["file"]["error"] > 0)
+ {
+ echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+ }
+ else
+ {
+ echo '<div class="plus">';
+ echo "Uploaded Successully";
+ echo '</div>';echo"<br/><b><u>Image Details</u></b><br/>";
+ echo "Name: " . $_FILES["file"]["name"] . "<br/>";
+ echo "Type: " . $_FILES["file"]["type"] . "<br/>";
+ echo "Size: " . ceil(($_FILES["file"]["size"] / 1024)) . " KB";
+ if (file_exists("upload/" . $_FILES["file"]["name"]))
+ {
+ unlink("upload/" . $_FILES["file"]["name"]);
+ }
+ else{
+ $pic=$_FILES["file"]["name"];
+ $conv=explode(".",$pic);
+ $ext=$conv['1'];
+ move_uploaded_file($_FILES["file"]["tmp_name"],"upload/". $user.".".$ext);
+ echo "Stored in as: " . "upload/".$user.".".$ext;
+ $url=$user.".".$ext;
+ $query="update userImage set url='$url', lastUpload=now() where user='$user'";
+ if($upl=$db->query($query)){
+echo "<br/>Saved to Database successfully";
+ }
+ }
+ }
+}else{
+ echo "File Size Limit Crossed 200 KB Use Picture Size less than 200 KB";
+}
+}
+?>
+<form action="" method="post" enctype="multipart/form-data">
+<?php
+$res=$db->query($pull);
+$pics=$res->fetch_assoc();
+echo '<div class="imgLow">';
+echo "<img src='upload/$pics[url]' alt='profile picture' width='80 height='64' class='doubleborder'/></div>";
+?>
+<input type="file" name="file" />
+<input type="submit" name="pupload" class="button" value="Upload"/>
+</form></span>
 
     
 </body>
