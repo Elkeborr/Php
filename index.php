868b03978ea98111c282b28f9d4a8b0963ce5549
Koen11 <r0718185@tudent.thomasmore.be>
2019-04-11 22:32:04 +0200

    aanpassing index voor uploads

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
+<?php 
+  $conn = new PDO("mysql:host=localhost;dbname=project_php", "root", "root", null);
+  $statement = $conn->prepare("SELECT * FROM images_with_fields");
+  $statement->execute();
+  $collection = $statement->fetchAll();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
     <title>Homepage</title>
 </head>
 <body>
@@ -13,6 +21,18 @@
 <a href="profiel.php">Profiel</a>
 <a href="login.php">login</a>
 <a href="register.php">Registreer</a>
+
+<form enctype="multipart/form-data" action="upload.php" method="POST" class="form"> 
+    <input type="file" name="image" capture="camera" required><br>
+    <br><textarea name="description" cols="40" rows="4" placeholder="Description" required></textarea><br>
+    <input type="submit" value="upload" name="upload" class="input">  
+</form>      
+  
+<div class="collection">
+    <?php foreach($collection as $c): ?>
+        <a href="detail.php?id=<?php echo $c['id']; ?>" class="collection__item" style="background-image:url(<?php echo $c['image']; ?>)"></a>
+    <?php endforeach; ?>
+</div>
     
 </body>
 </html>
