18eebcbe878e8bbb64bb6e2ca9d2ae876d28ba60
serafimay <49272435+serafimay@users.noreply.github.com>
2019-04-24 15:57:58 +0200

    sql

--- projectsql.sql
+++ projectsql.sql
@@ -1,16 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Gegenereerd op: 04 apr 2019 om 17:24
--- Serverversie: 5.7.23
--- PHP-versie: 7.2.10
+-- Host: localhost:3306
+-- Generation Time: Apr 24, 2019 at 01:57 PM
+-- Server version: 5.6.34-log
+-- PHP Version: 7.1.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `project_php`
@@ -19,7 +25,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `users`
+-- Table structure for table `images_with_fields`
+--
+
+CREATE TABLE `images_with_fields` (
+  `id` int(11) NOT NULL,
+  `image` varchar(100) COLLATE utf8_bin NOT NULL,
+  `image_text` text COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userimage`
+--
+
+CREATE TABLE `userimage` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `user` varchar(200) NOT NULL,
+  `url` text NOT NULL,
+  `lastUpload` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -32,39 +63,6 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Indexen voor geëxporteerde tabellen
---
-
---
--- Indexen voor tabel `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT voor geëxporteerde tabellen
---
-
---
--- AUTO_INCREMENT voor een tabel `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-
-
---DEEL Upload
-
---
--- Table structure for table `images_with_fields`
---
-
-CREATE TABLE `images_with_fields` (
-  `id` int(11) NOT NULL,
-  `image` varchar(100) COLLATE utf8_bin NOT NULL,
-  `image_text` text COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
 -- Indexes for dumped tables
 --
 
@@ -75,6 +73,18 @@ ALTER TABLE `images_with_fields`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `userimage`
+--
+ALTER TABLE `userimage`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -82,5 +92,13 @@ ALTER TABLE `images_with_fields`
 -- AUTO_INCREMENT for table `images_with_fields`
 --
 ALTER TABLE `images_with_fields`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;COMMIT;
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
