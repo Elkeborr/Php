860f1b5c39c8e5b8ddbdcd00d2f31c9574d4702d
Elkeborr <elkeborreij@student.thomasmore.be>
2019-04-24 10:09:29 +0200

    css_aanpassen

--- login.php
+++ login.php
@@ -39,10 +39,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>Login</title>
-  <link rel="stylesheet" href="css/login.css">
-  <link rel="stylesheet" href="css/style.css">
+  	<meta charset="UTF-8">
+  	<title>Login</title>
+  	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/login.css">
 
 </head>
 <body>
