533ad92c08a6268b10fd9d3ee05ec99026328057
Elkeborr <elkeborreij@student.thomasmore.be>
2019-04-25 09:09:25 +0200

    database_update

--- projectsql.sql
+++ projectsql.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Gegenereerd op: 25 apr 2019 om 07:05
+-- Gegenereerd op: 25 apr 2019 om 07:09
 -- Serverversie: 5.7.23
 -- PHP-versie: 7.2.10
 
@@ -13,6 +13,26 @@ SET time_zone = "+00:00";
 --
 -- Database: `project_php`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `images_with_fields`
+--
+
+CREATE TABLE `images_with_fields` (
+  `id` int(11) NOT NULL,
+  `image` varchar(255) COLLATE utf8_bin NOT NULL,
+  `image_text` text COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `images_with_fields`
+--
+
+INSERT INTO `images_with_fields` (`id`, `image`, `image_text`) VALUES
+(55, 'https://images.pexels.com/photos/1470171/pexels-photo-1470171.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Some nice plant from above'),
+(56, 'https://images.pexels.com/photos/707194/pexels-photo-707194.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Did some work today');
 
 -- --------------------------------------------------------
 
@@ -43,6 +63,12 @@ INSERT INTO `users` (`id`, `email`, `firstName`, `lastName`, `userName`, `passwo
 --
 
 --
+-- Indexen voor tabel `images_with_fields`
+--
+ALTER TABLE `images_with_fields`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexen voor tabel `users`
 --
 ALTER TABLE `users`
@@ -51,6 +77,12 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT voor geëxporteerde tabellen
 --
+
+--
+-- AUTO_INCREMENT voor een tabel `images_with_fields`
+--
+ALTER TABLE `images_with_fields`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT voor een tabel `users`
