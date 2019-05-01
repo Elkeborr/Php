72e9cebf17bad3a0c6d8fe4eec2cb9d6bf7eb454
serafimay <49272435+serafimay@users.noreply.github.com>
2019-05-01 15:26:12 +0200

    sql

--- projectsql.sql
+++ projectsql.sql
@@ -1,14 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Gegenereerd op: 25 apr 2019 om 20:16
--- Serverversie: 5.7.23
--- PHP-versie: 7.2.10
+-- Host: localhost:3306
+-- Generation Time: May 01, 2019 at 01:25 PM
+-- Server version: 5.6.34-log
+-- PHP Version: 7.1.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `project_php`
@@ -17,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `comments`
+-- Table structure for table `comments`
 --
 
 CREATE TABLE `comments` (
@@ -28,7 +36,7 @@ CREATE TABLE `comments` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Gegevens worden geëxporteerd voor tabel `comments`
+-- Dumping data for table `comments`
 --
 
 INSERT INTO `comments` (`id`, `text`, `user_id`, `post_id`) VALUES
@@ -37,7 +45,7 @@ INSERT INTO `comments` (`id`, `text`, `user_id`, `post_id`) VALUES
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `followers`
+-- Table structure for table `followers`
 --
 
 CREATE TABLE `followers` (
@@ -47,7 +55,7 @@ CREATE TABLE `followers` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Gegevens worden geëxporteerd voor tabel `followers`
+-- Dumping data for table `followers`
 --
 
 INSERT INTO `followers` (`id`, `user_id1`, `user_id2`) VALUES
@@ -58,7 +66,7 @@ INSERT INTO `followers` (`id`, `user_id1`, `user_id2`) VALUES
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `images_with_fields`
+-- Table structure for table `images_with_fields`
 --
 
 CREATE TABLE `images_with_fields` (
@@ -69,7 +77,7 @@ CREATE TABLE `images_with_fields` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Gegevens worden geëxporteerd voor tabel `images_with_fields`
+-- Dumping data for table `images_with_fields`
 --
 
 INSERT INTO `images_with_fields` (`id`, `image`, `image_text`, `user_id`) VALUES
@@ -78,24 +86,34 @@ INSERT INTO `images_with_fields` (`id`, `image`, `image_text`, `user_id`) VALUES
 (57, 'https://images.pexels.com/photos/311458/pexels-photo-311458.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', 'Lolzz', 28),
 (58, 'https://images.pexels.com/photos/1477166/pexels-photo-1477166.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', 'hihihi', 23),
 (59, 'https://images.pexels.com/photos/38136/pexels-photo-38136.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', '', 28),
-(60, 'https://images.pexels.com/photos/796620/pexels-photo-796620.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', '', 23);
+(60, 'https://images.pexels.com/photos/796620/pexels-photo-796620.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', '', 23),
+(61, 'images/FB_20160508_14_46_41_Saved_Picture 1.jpg', 'seraf', 0),
+(62, 'images/post_images/WmDev_636017832951914926.jpg', 'hello people', 26);
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `profile_images`
+-- Table structure for table `profile_images`
 --
 
 CREATE TABLE `profile_images` (
   `id` int(11) NOT NULL,
   `image` varchar(255) NOT NULL,
-  `user_id` int(11) NOT NULL
+  `user_id` int(11) NOT NULL,
+  `image_text` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `profile_images`
+--
+
+INSERT INTO `profile_images` (`id`, `image`, `user_id`, `image_text`) VALUES
+(3, 'images/post_images/FB_20160623_21_34_42_Saved_Picture.jpg', 0, '');
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -108,7 +126,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Gegevens worden geëxporteerd voor tabel `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `email`, `firstName`, `lastName`, `userName`, `password`) VALUES
@@ -117,69 +135,69 @@ INSERT INTO `users` (`id`, `email`, `firstName`, `lastName`, `userName`, `passwo
 (28, 'elliot.doms@gmail.com', 'Elliot', 'Doms', 'ElliotDoms', '$2y$12$nPhBtAoNQOToa6OyO47rseHZ8tJ7YnUE9qU6laXUQX7Nz12f9kpAm');
 
 --
--- Indexen voor geëxporteerde tabellen
+-- Indexes for dumped tables
 --
 
 --
--- Indexen voor tabel `comments`
+-- Indexes for table `comments`
 --
 ALTER TABLE `comments`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexen voor tabel `followers`
+-- Indexes for table `followers`
 --
 ALTER TABLE `followers`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexen voor tabel `images_with_fields`
+-- Indexes for table `images_with_fields`
 --
 ALTER TABLE `images_with_fields`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexen voor tabel `profile_images`
+-- Indexes for table `profile_images`
 --
 ALTER TABLE `profile_images`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexen voor tabel `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT voor geëxporteerde tabellen
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT voor een tabel `comments`
+-- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
 --
--- AUTO_INCREMENT voor een tabel `followers`
+-- AUTO_INCREMENT for table `followers`
 --
 ALTER TABLE `followers`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
 --
--- AUTO_INCREMENT voor een tabel `images_with_fields`
+-- AUTO_INCREMENT for table `images_with_fields`
 --
 ALTER TABLE `images_with_fields`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 --
--- AUTO_INCREMENT voor een tabel `profile_images`
+-- AUTO_INCREMENT for table `profile_images`
 --
 ALTER TABLE `profile_images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
--- AUTO_INCREMENT voor een tabel `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
