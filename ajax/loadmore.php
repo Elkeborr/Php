e80bd563b255ff2c7645443647fb4b8d6eb1abf3
Elkeborr <elkeborreij@student.thomasmore.be>
2019-05-06 12:31:09 +0200

    load_more_werkt

--- ajax/loadmore.php
+++ ajax/loadmore.php
@@ -1,27 +1,47 @@
 <?php
 
 require_once '../bootstrap.php';
+session_start();
+$row = $_POST['row'];
 
-if (!empty($_GET)) {
-    try {
-        $e = Post::getAll();
-        if ($e == true) {
-            $result = [
-                    'status' => 'succes',
-                    'message' => 'Hier zijn de afbeeldingen',
-                    ];
-        } else {
-            $result = [
-                    'status' => 'false',
-                    'message' => 'Oepsie poepsie',
-                    ];
-        }
-    } catch (Throwable $t) {
-        $result = [
-            'status' => 'error',
-            'message' => 'er is iet fout gelopen',
-            ];
-    }
+$rowperpage = 3;
 
-    echo json_encode($result);
+$conn = Db::getInstance();
+
+// selecting posts
+$stm = $conn->prepare("SELECT id FROM users WHERE email = '".$_SESSION['email']."'");
+$stm->execute();
+$id = $stm->fetch(PDO::FETCH_COLUMN);
+
+$posts = $conn->prepare("SELECT images_with_fields.id,images_with_fields.image,images_with_fields.image_text,images_with_fields.user_id
+FROM images_with_fields,followers WHERE followers.user_id1=:id AND followers.user_id2=images_with_fields.user_id 
+UNION SELECT images_with_fields.id,images_with_fields.image,images_with_fields.image_text,images_with_fields.user_id FROM images_with_fields,followers 
+WHERE  images_with_fields.user_id =:id  LIMIT $row,$rowperpage");
+$posts->bindValue(':id', $id);
+$posts->execute();
+
+$result = $posts->fetchAll(PDO::FETCH_ASSOC);
+
+$html = '';
+
+foreach ($result as $r) {
+    $id = $r['id'];
+    $image = $r['image'];
+    $image_text = $r['image_text'];
+    //$date = $r['date'];
+
+    // Creating HTML structure
+    $html .= '<div class="collection__item">';
+    $html .= '<a href="detail.php?id='.$id.'"><img class="collection--image" src="'.$image.'" alt="Post"></a>';
+    $html .= '<div class="item--container">';
+    $html .= '<div class="profile--small ">';
+    $html .= ' <img class="profile--imageSmall" src="">';
+    $html .= '</div>';
+    $html .= '<p>'.$image_text.'</p>';
+    // $html .= '<p>'.$date.'</p>';
+    $html .= '<button>Like</button>';
+    $html .= '</div>';
+    $html .= '</div>';
 }
+
+echo $html;
--- classes/post.php
+++ classes/post.php
@@ -63,7 +63,7 @@ class Post
         $statement = $conn->prepare('SELECT images_with_fields.id,images_with_fields.image,images_with_fields.image_text,images_with_fields.user_id
                 FROM images_with_fields,followers WHERE followers.user_id1=:id AND followers.user_id2=images_with_fields.user_id 
                 UNION SELECT images_with_fields.id,images_with_fields.image,images_with_fields.image_text,images_with_fields.user_id FROM images_with_fields,followers 
-                WHERE  images_with_fields.user_id =:id  LIMIT 20');
+                WHERE  images_with_fields.user_id =:id  LIMIT 3');
 
         $statement->bindValue(':id', $id);
         $statement->execute();
