9042ce3c8fc85bc6c523814e2dc04c00aebaee63
Elkeborr <elkeborreij@student.thomasmore.be>
2019-05-09 08:32:11 +0200

    update_bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,8 @@
 <?php
-    spl_autoload_register(function($class){
-        require_once(__DIR__ . DIRECTORY_SEPARATOR . 
-        "classes" . DIRECTORY_SEPARATOR . $class . ".php");
+
+//session starten voor overal te gebruiken
+   session_start();
+    spl_autoload_register(function ($class) {
+        require_once __DIR__.DIRECTORY_SEPARATOR.
+        'classes'.DIRECTORY_SEPARATOR.$class.'.php';
     });
